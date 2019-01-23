20cb85b331ecb7164a5b1f4c8f4c14d682bc695f
Jacob Reinhold <jacob.reinhold@jhu.edu>
2019-01-23 10:02:31 -0500

    add more comprehensive unittests

--- tests/test_exec.py
+++ tests/test_exec.py
@@ -82,14 +82,22 @@ class TestCLI(unittest.TestCase):
         self.assertEqual(retval, 0)
 
     def test_nn_train_unet_cli(self):
-        args = self.train_args + f'-o {self.out_dir}/unet.mdl -na unet -ne 1 -nl 1 -cbp 1 -ps 16 -bs 2 --net3d'.split()
+        args = self.train_args + (f'-o {self.out_dir}/unet.mdl -na unet -ne 1 -nl 3 -cbp 1 -ps 16 -bs 2 --net3d '
+                                  f'-ocf {self.jsonfn}').split()
         retval = nn_train(args)
+        self.assertEqual(retval, 0)
+        self.__modify_ocf(self.jsonfn, multi=2)
+        retval = nn_predict([self.jsonfn])
         self.assertEqual(retval, 0)
 
     def test_nn_train_unet_multimodal_cli(self):
         train_args = f'-s {self.train_dir}/1/ {self.train_dir}/1/ -t {self.train_dir}/2/ {self.train_dir}/2/'.split()
-        args = train_args + f'-o {self.out_dir}/unet.mdl -na unet -ne 1 -nl 3 -cbp 1 -ps 16 -bs 2 --tiff'.split()
+        args = train_args + (f'-o {self.out_dir}/unet.mdl -na unet -ne 1 -nl 3 -cbp 1 -ps 16 -bs 2 --tiff '
+                             f'-ocf {self.jsonfn}').split()
         retval = nn_train(args)
+        self.assertEqual(retval, 0)
+        self.__modify_ocf(self.jsonfn, multi=2)
+        retval = nn_predict([self.jsonfn])
         self.assertEqual(retval, 0)
 
     def test_nn_nconv_multimodal_cli(self):
