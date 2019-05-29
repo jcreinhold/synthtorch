dec6f5f106dc0d98027b99738cb5e35857de7a0d
Jacob Reinhold <jcreinhold@gmail.com>
2019-05-29 12:47:31 -0400

    fix 3d patch based prediction

--- synthtorch/learn/predict.py
+++ synthtorch/learn/predict.py
@@ -70,47 +70,48 @@ class Predictor:
         """ 3d patch-by-patch based prediction """
         if img.ndim == 3: img = img[np.newaxis, ...]
         # pad image to get full image coverage in patch-based processing
-        sz = img.shape
-        pad = [int((psz // 2) * np.ceil(sz[i]/(psz // 2)) - sz[i]) for i, psz in enumerate(self.patch_size, 1)]
+        sz, psz = img.shape, self.patch_size
+        pad = [int((ps // 2) * np.ceil(sz[i]/(ps // 2)) - sz[i]) for i, ps in enumerate(psz, 1)]
         img = np.asarray(F.pad(torch.from_numpy(img[np.newaxis,...]),
-                               (0, pad[2], 0, pad[1], 0, pad[0]), mode='circular')[0])
+                               (0, pad[2] if psz[2] != sz[3] else 0,
+                                0, pad[1] if psz[1] != sz[2] else 0,
+                                0, pad[0] if psz[0] != sz[1] else 0), mode='circular')[0])
         out_img = np.zeros((self.n_output,) + img.shape[1:])
         count_mtx = np.zeros(img.shape[1:])
         x, y, z = self._get_overlapping_3d_idxs(self.patch_size, img)
-        dec_idxs = np.floor(np.percentile(np.arange(x.shape[0]), np.arange(0, 101, 10)))
-        pct_complete = 0
-        j = 0
         # The below for-loop handles collecting overlapping patches and putting
         # them into a batch format that pytorch models expect (i.e., [N,C,H,W,D])
         # and running the batch through the network (storing the results in out_img).
+        j = 0
+        batch, idxs = [], []
         for i, (xx, yy, zz) in enumerate(zip(x, y, z)):
-            if i in dec_idxs:
-                logger.info(f'{pct_complete}% Complete')
-                pct_complete += 10
-            count_mtx[xx, yy, zz] = count_mtx[xx, yy, zz] + 1
-            if j == 0:
-                batch = np.zeros((self.batch_size,) + img[:, xx, yy, zz].shape, dtype=np.float32)
-                batch_idxs = [(xx, yy, zz)]
-                batch[j, ...] = img[:, xx, yy, zz]
-                j += 1
-            elif j < self.batch_size:
-                batch_idxs.append((xx, yy, zz))
-                batch[j, ...] = img[:, xx, yy, zz]
+            if j < self.batch_size:
+                idxs.append((xx, yy, zz))
+                batch.append(img[:, xx, yy, zz])
                 j += 1
             else:
-                batch = torch.from_numpy(batch).to(self.device)
-                predicted = np.zeros((self.batch_size,self.n_output,) + batch.shape[2:])
-                for _ in range(nsyn):
-                    predicted += self._fwd(batch, temperature_map) / nsyn
-                for ii, (bx, by, bz) in enumerate(batch_idxs):
-                    out_img[:, bx, by, bz] = out_img[:, bx, by, bz] + predicted[ii, ...]
-                j = 0
+                self.__batch_3d_proc(batch, idxs, nsyn, temperature_map, out_img, count_mtx)
+                # restart new batch
+                batch, idxs = [img[:, xx, yy, zz]], [(xx, yy, zz)]
+                j = 1
+        if np.any(count_mtx == 0):
+            self.__batch_3d_proc(batch, idxs, nsyn, temperature_map, out_img, count_mtx)
         if np.any(count_mtx == 0):
             logger.warning(f'Part of the synthesized image not covered ({np.sum(count_mtx == 0)} voxels)')
             count_mtx[count_mtx == 0] = 1  # avoid division by zero
         out_img /= count_mtx
         out_img = out_img[:,:sz[1],:sz[2],:sz[3]]
         return out_img
+
+    def __batch_3d_proc(self, batch, idxs, nsyn, temperature_map, out_img, count_mtx):
+        bs = len(batch)
+        batch = torch.from_numpy(np.stack(batch)).to(self.device)
+        predicted = np.zeros((bs,self.n_output,) + batch.shape[2:])
+        for _ in range(nsyn):
+            predicted += self._fwd(batch, temperature_map) / nsyn
+        for ii, (bx, by, bz) in enumerate(idxs):
+            out_img[:, bx, by, bz] += predicted[ii, ...]
+            count_mtx[bx, by, bz] += 1
 
     def slice_predict(self, img:np.ndarray, nsyn:int=1, temperature_map:bool=False, calc_var:bool=False) -> np.ndarray:
         """ slice-by-slice based prediction """
@@ -155,12 +156,15 @@ class Predictor:
             out = self.model.predict(img, temperature_map).cpu().detach().numpy()
         return out
 
-    @staticmethod
-    def _get_overlapping_3d_idxs(psz, img):
-        indices = [torch.from_numpy(idxs).unfold(0, psz[0], psz[0]//2).unfold(1, psz[1], psz[1]//2).unfold(2, psz[2], psz[2]//2)
-                   for idxs in np.indices(img.shape[1:])]
+    def _get_overlapping_3d_idxs(self, psz, img):
+        indices = [self.__unfold(torch.from_numpy(idxs), psz, img) for idxs in np.indices(img.shape[1:])]
         x, y, z = [np.asarray(idxs.contiguous().view(-1, psz[0], psz[1], psz[2])) for idxs in indices]
         return x, y, z
+
+    def __unfold(self, idxs, psz, img):
+        return idxs.unfold(0, psz[0], psz[0]//(2 if psz[0] != img.shape[1] else 1))\
+                   .unfold(1, psz[1], psz[1]//(2 if psz[1] != img.shape[2] else 1))\
+                   .unfold(2, psz[2], psz[2]//(2 if psz[2] != img.shape[3] else 1))
 
     def _batch2d(self, img, out_img, i, nsyn, temperature_map, bs=None):
         bs = bs or self.batch_size
